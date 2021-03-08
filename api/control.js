e5ecd5a328a372e84b233e186a03142b1f6e17aa
jorgadev <radovanjorgic14@gmail.com>
2021-03-08 16:12:33 +0100

    Control added

--- api/control.js
+++ api/control.js
@@ -1,9 +1,14 @@
-const relay1 = 0;
-const relay2 = 0;
-const relay3 = 0;
-const sensor1 = 0;
-const sensor2 = 0;
-const sensor3 = 0;
+var sensor = require("node-dht-sensor");
+var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+
+var RELAY_COOLING = new Gpio(21, 'out'); 
+var RELAY_HEATING = new Gpio(20, 'out'); 
+var RELAY_PUMP = new Gpio(16, 'out');
+var RELAY_FAN = new Gpio(12, 'out');
+var RELAY_STEAM = new Gpio(1, 'out');
+
+var DHT_TYPE=22;
+var DHT_PIN = 4;
 
 const optionA = [
   "clear sky",
@@ -34,6 +39,7 @@ module.exports = {
     // else doesn't run anything
     else {
       console.log("opcija A");
+      this.measure();
     }
   },
 
@@ -41,4 +47,23 @@ module.exports = {
     // stop all relays and maybe sensors
     console.log("stopped");
   },
+
+  measure: function() {
+    sensor.read(DHT_TYPE, DHT_PIN, function(err, temperature, humidity) {
+    if (!err) {
+      const temp = Math.floor(temperature);
+      const hum = Math.floor(humidity);
+      console.log(`temp: ${temp}, hum: ${Math.floor(hum)}`);
+      if(temp >= 27){
+        RELAY_COOLING.writeSync(1); //set pin state to 1 (turn LED on)
+        RELAY_HEATING.writeSync(0);
+      }else{
+        RELAY_COOLING.writeSync(0); 
+        RELAY_HEATING.writeSync(1); 
+      }
+    }else{
+    console.log(err);
+    }
+  }); 
+  }
 };
