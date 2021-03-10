68ea9f860a6d22076a4cf3c136906ee81b7208f3
jorgadev <radovanjorgic14@gmail.com>
2021-03-10 09:20:12 +0100

    Heating and cooling functionality

--- api/control.js
+++ api/control.js
@@ -1,19 +1,19 @@
 // Import needed libraries
-// const sensor = require("node-dht-sensor");
-// const Gpio = require("onoff").Gpio;
+const sensor = require("node-dht-sensor");
+const Gpio = require("onoff").Gpio;
 const mongoUtil = require("./mongo-util");
 
 // Database reference
 const db = mongoUtil.getDb();
 
 // Inputs and outputs
-// const RELAY_COOLING = new Gpio(21, "out");
-// const RELAY_HEATING = new Gpio(20, "out");
-// const RELAY_PUMP = new Gpio(16, "out");
-// const RELAY_FAN = new Gpio(12, "out");
-// const RELAY_STEAM = new Gpio(1, "out");
-// const DHT_TYPE = 22;
-// const DHT_PIN = 4;
+const RELAY_COOLING = new Gpio(21, "out");
+const RELAY_HEATING = new Gpio(20, "out");
+const RELAY_PUMP = new Gpio(16, "out");
+const RELAY_FAN = new Gpio(12, "out");
+const RELAY_STEAM = new Gpio(1, "out");
+const DHT_TYPE = 22;
+const DHT_PIN = 4;
 
 // All possible data from api divided in three situations
 const sunny = [
@@ -29,50 +29,76 @@ const misty = ["mist"];
 // Module to export
 module.exports = {
   start: function () {
-    // RELAY_COOLING.writeSync(0);
-    // RELAY_HEATING.writeSync(0);
-    // RELAY_PUMP.writeSync(0);
-    // RELAY_FAN.writeSync(0);
-    // RELAY_STEAM.writeSync(0);
-    // sensor.read(DHT_TYPE, DHT_PIN, function (err, temperature, humidity) {
-    //   if (!err) {
-    //     const temp = Math.floor(temperature);
-    //     const hum = Math.floor(humidity);
-    //     console.log(
-    //       `Relays reset and temperature (${temp}) and humidity (${hum}) read`
-    //     );
-    //     const startObject = {
-    //       temperature: temp,
-    //       humidity: hum,
-    //       heating: 0,
-    //       cooling: 0,
-    //       pump: 0,
-    //       fan: 0,
-    //       steam: 0,
-    //     };
-    //     mongoUtil.insertIntoDb("chamber", startObject);
-    //   } else {
-    //     console.log(err);
-    //   }
-    // });
+    RELAY_COOLING.writeSync(0);
+    RELAY_HEATING.writeSync(0);
+    RELAY_PUMP.writeSync(0);
+    RELAY_FAN.writeSync(0);
+    RELAY_STEAM.writeSync(0);
+    sensor.read(DHT_TYPE, DHT_PIN, function (err, temperature, humidity) {
+      if (!err) {
+        const temp = Math.floor(temperature);
+        const hum = Math.floor(humidity);
+        console.log(
+          `Relays reset and temperature (${temp}) and humidity (${hum}) read`
+        );
+        const startObject = {
+          temperature: temp,
+          humidity: hum,
+          heating: 0,
+          cooling: 0,
+          pump: 0,
+          fan: 0,
+          steam: 0,
+        };
+        mongoUtil.insertIntoDb("chamber", startObject);
+      } else {
+        console.log(err);
+      }
+    });
+    this.measure();
   },
 
-  // measure: function () {
-  //   sensor.read(DHT_TYPE, DHT_PIN, function (err, temperature, humidity) {
-  //     if (!err) {
-  //       const temp = Math.floor(temperature);
-  //       const hum = Math.floor(humidity);
-  //       console.log(`temp: ${temp}, hum: ${Math.floor(hum)}`);
-  //       if (temp >= 26) {
-  //         RELAY_COOLING.writeSync(1); //set pin state to 1 (turn LED on)
-  //         RELAY_HEATING.writeSync(0);
-  //       } else {
-  //         RELAY_COOLING.writeSync(0);
-  //         RELAY_HEATING.writeSync(1);
-  //       }
-  //     } else {
-  //       console.log(err);
-  //     }
-  //   });
-  // },
+  measure: function () {
+    mongoUtil.getLastRow("data").then((apiData) => {
+      sensor.read(DHT_TYPE, DHT_PIN, function (err, temperature, humidity) {
+        if (!err) {
+          const temp = Math.floor(temperature);
+          // Compare api and chamber data and control relays
+          switch (checkTemperature(apiData.temp, temp)) {
+            case 0:
+              console.log("nic ne vklapljaj");
+              break;
+            case 1:
+              console.log("vklopi grelec");
+              RELAY_COOLING.writeSync(0);
+              RELAY_HEATING.writeSync(1);
+              break;
+            case -1:
+              console.log("vklopi hladilnik");
+              RELAY_COOLING.writeSync(1);
+              RELAY_HEATING.writeSync(0);
+              break;
+            default:
+              console.log("napaka pri izracunu temperature");
+          }
+        } else {
+          console.log(err);
+        }
+      });
+
+      // checkDescription(apiData.description);
+    });
+  },
+};
+
+const checkTemperature = (apiTemp, chamberTemp) => {
+  if (chamberTemp >= apiTemp - 1 && chamberTemp <= apiTemp + 1) {
+    return 0;
+  } else if (chamberTemp > apiTemp + 1) {
+    return -1;
+  } else if (chamberTemp < apiTemp - 1) {
+    return 1;
+  } else {
+    return null;
+  }
 };
